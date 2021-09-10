5fe8f5fe67a7e53ba9accfcfa8313f0d483ed068
AkshaiKumaar <83781292+AkshaiKumaar@users.noreply.github.com>
2021-09-10 12:13:21 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <div style="clear:both;color:#aaa; padding:20px;">
+
+    	<hr /><center>&copy; 2013 <a target="_blank" href="http://optimumlinkup.com.ng">Optimum Linkup Computer Institute</a></center>
+
+    </div>
